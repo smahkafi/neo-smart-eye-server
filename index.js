502bab234f695adca545299fabe3f0e00021b932
smahkafi <sakafi.m@gmail.com>
2021-11-11 21:48:46 +0600

    delete method add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId
 // const admin = require("firebase-admin");
 
 const port = process.env.PORT || 5000;
@@ -97,6 +98,16 @@ async function run() {
             const result = await ordersCollections.find({ email }).toArray();
             res.json(result);
         })
+
+        // orders deleted by order id
+        app.delete("/orders/:id", async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollections.deleteOne(query);
+            console.log(result);
+            res.send(result);
+        });
 
 
         // users check by admin 
